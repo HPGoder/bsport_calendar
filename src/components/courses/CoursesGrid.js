9974cd9221ba3d683d07a6734f7a9436e873a46e
HPGoder <71547230+HPGoder@users.noreply.github.com>
2021-01-13 13:09:02 +0100

    Issuance
    Commit for review, please not that I did not include the Member card component since for some reasons I cannot perform a request to the members using an array thus, the method has to perform n API calls for n members attending a course...

--- src/components/courses/CoursesGrid.js
+++ src/components/courses/CoursesGrid.js
@@ -4,7 +4,7 @@ import {useState} from 'react';
 import CourseDetailsPanel from './CourseDetailsPanel'
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
+
 const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 2,
